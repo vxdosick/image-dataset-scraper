92b8176a83c4d17a104ba362bf7926e4bea6d905
vxdosick <vadumfotenuk@gmail.com>
2025-12-21 19:24:50 +0100

    Create starting config

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="app/templates")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/", response_class = HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/dataset", response_class=HTMLResponse)
+async def generate_dataset(request: Request, query: str = Form(...), count: int = Form(...)):
+    return templates.TemplateResponse("result.html", {"request": request, "query": query, "count": count})
